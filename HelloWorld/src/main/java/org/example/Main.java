8cac9a90859ea6b4071ff7ae688815e2c9eaba1e
emily doswell <emilydoswell@yahoo.co.uk>
2022-10-12 09:28:07 +0100

    first commit

---
+++ HelloWorld/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ *@emily
+ *email: emilydoswell@yahoo.co.uk
+ *date: 11/10/2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ HelloWorldJava/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello it's Emily");
+
+        int counter;
+        boolean isDone = true;
+
+        if(isDone) {
+           System.out.println("It's true!");
+        }
+    }
+}
