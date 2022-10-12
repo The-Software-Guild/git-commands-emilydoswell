9370e81591adfa6d2f845a981a7ab88e47ad57af
emily doswell <emilydoswell@yahoo.co.uk>
2022-10-12 14:39:06 +0100

    completed YourLifeInMovies

--- Practice Programming/src/com/sg/foundations/flowcontrol/ifs/YourLifeInMovies.java
+++ Practice Programming/src/com/sg/foundations/flowcontrol/ifs/YourLifeInMovies.java
@@ -1,14 +1,39 @@
 package com.sg.foundations.flowcontrol.ifs;
 
+import java.util.Scanner;
+
 public class YourLifeInMovies {
     public static void main(String[] args) {
         // Write a program that displays a different message based on the year someone was born to make them feel old.
-        // If they were born before 2005, print out that Pixar's 'Up' came out over a decade ago.
-        // If they were born before 1995, print out that the first Harry Potter came out over 15 years ago.
-        // If they were born before 1985, print out Space Jam came out not last decade, but the one before THAT.
-        // If they were born before 1975, print out that the original Jurassic Park release is closer to the first lunar landing than it is to today.
-        // If they were born before 1965, print out that the MASH TV series has been around for almost half a century!
-        // Note: A person who is born in 1980 should display three messages, one for being born before 2005, one for 1995, and finally for 1985.
+        System.out.println("What year were you born?");
 
+        //Create Scanner
+        Scanner input = new Scanner(System.in);
+        int yearOfBirth = input.nextInt();
+
+        // If they were born before 2005, print out that Pixar's 'Up' came out over a decade ago.
+        if (yearOfBirth < 2005) {
+            System.out.println("Did you know that Pixar's 'Up' came out over a decade ago ");
+        }
+
+        // If they were born before 1995, print out that the first Harry Potter came out over 15 years ago.
+        if (yearOfBirth < 1995) {
+            System.out.println("Did you know that the first Harry Potter came out over 15 years ago ");
+        }
+
+        // If they were born before 1985, print out Space Jam came out not last decade, but the one before THAT.
+        if (yearOfBirth < 1985) {
+            System.out.println("Did you know that Space Jam came out not last decade, but the one before THAT.");
+        }
+
+        // If they were born before 1975, print out that the original Jurassic Park release is closer to the first lunar landing than it is to today.
+        if (yearOfBirth < 1975) {
+            System.out.println("Did you know that the the original Jurassic Park release is closer to the first lunar landing than it is to today.");
+        }
+
+        // If they were born before 1965, print out that the MASH TV series has been around for almost half a century!
+        if (yearOfBirth < 1965) {
+            System.out.println("Did you know that the MASH TV series has been around for almost half a century!");
+        }
     }
 }
