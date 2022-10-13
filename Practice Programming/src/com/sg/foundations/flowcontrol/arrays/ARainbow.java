c29ce9697d2f210591f12d877beb9b8cd53d801b
emily doswell <emilydoswell@yahoo.co.uk>
2022-10-13 11:40:30 +0100

    Completed rainbow task

---
+++ Practice Programming/src/com/sg/foundations/flowcontrol/arrays/ARainbow.java
@@ -0,0 +1,19 @@
+package com.sg.foundations.basics.arrays;
+
+public class ARainbow {
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+        System.out.println(colors[5]);
+        System.out.println(colors[6]);
+
+    }
+}
+
+// Type out the following code to create and print a String[].
+// You'll notice that something doesn't quite match up. Fix the code to print out the colors in true ROYGBIV order!
