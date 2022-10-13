390d2c0198e68625b2c7ae682181297c57193f8d
emily doswell <emilydoswell@yahoo.co.uk>
2022-10-13 16:41:39 +0100

    dog

---
+++ Assessments/Basic Programming Concepts/src/DogGenetics.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DogGenetics {
+    public static void main(String[] args) {
+        // Ask user for dog name
+        System.out.println("What is your dog's name?");
+
+        // Create Scanner
+        Scanner input = new Scanner(System.in);
+        String userInput = input.next();
+
+        // Print response
+        System.out.println("Well then, I have this highly reliable report on " + userInput + "'s prestigious background right here.");
+        System.out.println(userInput + " is:");
+
+        // Create array of dog breeds
+        String[] dogBreeds = {"St. Bernard", "Chihuahua", "Dramatic RedNosed Asian Pug", "Common Cur", "King Doberman"};
+
+        for (int i = 0; i < dogBreeds.length; i++) {
+            Random rng = new Random();
+            int randomNumber = rng.nextInt(100) + 1;
+
+            System.out.println(randomNumber + "% " + dogBreeds[i]);
+        }
+
+        System.out.println("Wow, that's QUITE the dog!");
+    }
+}
