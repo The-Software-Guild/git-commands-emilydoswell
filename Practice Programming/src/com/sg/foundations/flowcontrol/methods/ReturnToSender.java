e67416e0ed600715d31fe22c06f57cc2c5fc216d
emily doswell <emilydoswell@yahoo.co.uk>
2022-10-13 10:31:38 +0100

    Created code for ReturnToSender

---
+++ Practice Programming/src/com/sg/foundations/flowcontrol/methods/ReturnToSender.java
@@ -0,0 +1,40 @@
+package com.sg.foundations.basics.methods;
+
+public class ReturnToSender {
+    public static void main(String[] args) {
+
+        ??? aMystery = mystery();
+        ??? totallyUnexpected = unexpected();
+        ??? aSurprise = surprise();
+        ??? itsClassified = classified();
+        ??? aSecret = secret();
+
+        System.out.println("The methods have returned! Their results...\n");
+        System.out.println("Mysterious: " + aMystery);
+        System.out.println("    Secret: " + aSecret);
+        System.out.println("Surprising: " + aSurprise);
+        System.out.println("Classified: " + itsClassified);
+        System.out.println("Unexpected: " + totallyUnexpected);
+
+    }
+
+    public static int secret(){
+        return 42;
+    }
+
+    public static double surprise(){
+        return 3.14;
+    }
+
+    public static char mystery(){
+        return 'X';
+    }
+
+    public static boolean classified(){
+        return true;
+    }
+
+    public static String unexpected(){
+        return "Spanish Inquisition";
+    }
+}
