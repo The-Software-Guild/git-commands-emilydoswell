e03fefb57257fb17cddc5157d7372eec3e193df4
emily doswell <emilydoswell@yahoo.co.uk>
2022-10-14 10:13:45 +0100

    summative sums

--- Assessments/Basic Programming Concepts/src/SummativeSums.java
+++ Assessments/Basic Programming Concepts/src/SummativeSums.java
@@ -1,17 +1,27 @@
 public class SummativeSums {
     public static void main(String[] args) {
+        // Initialise arrays
         int[] ex1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
-        int[] ex2 = { 999, -60, -77, 14, 160, 301 }
+        int[] ex2 = { 999, -60, -77, 14, 160, 301 };
         int[] ex3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99 };
 
-        for (int i = 0; i < array.length; i++) {
-            int sum = 0;
-            sum += array[i];
+        // Print sum of each array 
+        System.out.println("#1 Array Sum: " + totalArray(ex1));
+        System.out.println("#2 Array Sum: " + totalArray(ex2));
+        System.out.println("#3 Array Sum: " + totalArray(ex3));
         }
-        System.out.println("#" + (array[i]+1) + "Array Sum: " + );
 
-        System.out.println("#2 Array Sum: ");
-        System.out.println("#3 Array Sum: ");
+        // Create function add all elements in the array
+        public static int totalArray(int[] array) {
+            // Initialise counter
+            int sum = 0;
 
-    }
+            // Loop through array and add each element to counter
+            for (int i = 0; i < array.length; i++) {
+                sum += array[i];
+            }
+
+            // Return sum
+            return sum;
+        }
 }
